2224b594e165f6a2f0d4afb1f803afc6a5e322e9
Pankaj <pankajsangle15@gmail.com>
2023-06-18 22:20:07 +0530

    test 101 with Bhagwan Remote

---
+++ first.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"This is in main computer.";
+
+    return 0;
+}
---
+++ sec.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"This is in main comp";
+
+    return 0;
+}
